47f5b6de8b8ce7862bd8ef730a064ffe124af6ee
Ethan Stockbauer <stockbae@matc.edu>
2025-12-02 18:48:06 -0600

    complete activity 10: order checkout frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import ProductDetail from './components/ProductDetail';
 import './App.css';
 import CartButton from "./components/Cart/CartButton";
 import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
+import OrderSuccess from "./components/Checkout/OrderSuccess";
+
 
 
 function App() {
@@ -118,6 +121,17 @@ function App() {
               path="/products/:id"
               element={<ProductDetail addToCart={addToCart} />}
             />
+            <Route
+              path="/checkout"
+              element={
+                <Checkout
+                  cartItems={cartItems}
+                  cartTotal={getCartTotal()}
+                  clearCart={clearCart}
+                />
+              }
+            />
+            <Route path="/order/success" element={<OrderSuccess />} />
           </Routes>
         </main>
 
